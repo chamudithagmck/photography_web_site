694413f8cb3e863113eb01d1eb24c200451192b0
G.M.Chamuditha Karunarathna <chamudithakarunarathna06@gmail.com>
2026-08-12 21:20:01 +0530

    springboot initializer was added
    Add Dependencies,
    
    Spring Web: To build RESTful APIs.
    Spring Data JPA: To handle database operations using Java objects.
    MySQL Driver: (Or PostgreSQL Driver) To allow Spring to talk to your database.
    Lombok: (Optional but highly recommended) Saves you from writing repetitive getters/setters in your classes.
    
    Click "Generate", download the .zip file, and extract it to your project folder.

---
+++ stable/stable/src/test/java/com/photography/stable/StableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photography.stable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
